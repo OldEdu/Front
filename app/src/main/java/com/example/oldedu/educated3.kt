2b3f90c2aa552f1f977dd8f9ea8cf1ad4773337c
knxxxn <20211308@sungshin.ac.kr>
2023-01-16 23:32:15 +0900

    4page add

---
+++ app/src/main/java/com/example/oldedu/educated3.kt
@@ -0,0 +1,11 @@
+package com.example.oldedu
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class educated3 : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_educated3)
+    }
+}
