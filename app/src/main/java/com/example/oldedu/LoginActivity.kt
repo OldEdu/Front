3f871571fd2866ece26ab6035dc053126dea9a58
kaayoung <20190856@sungshin.ac.kr>
2023-01-28 18:33:15 +0900

    modify sign up UI

--- app/src/main/java/com/example/oldedu/LoginActivity.kt
+++ app/src/main/java/com/example/oldedu/LoginActivity.kt
@@ -1,11 +1,22 @@
 package com.example.oldedu
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 
 class LoginActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
+
+        val btnSignUp = findViewById<Button>(R.id.btn_signUp)
+
+        fun moveToSignupPage() {
+            val intent = Intent(this, SignupActivity::class.java)
+            startActivity(intent)
+        }
+
+        btnSignUp.setOnClickListener{moveToSignupPage()}
     }
 }
--- app/src/main/java/com/example/oldedu/SignupActivity.kt
+++ app/src/main/java/com/example/oldedu/SignupActivity.kt
@@ -1,11 +1,22 @@
 package com.example.oldedu
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 
 class SignupActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_signup)
+
+        val btnSignUpSubmit = findViewById<Button>(R.id.btn_signupSubmit)
+
+        fun moveToSignupPage() {
+            val intent = Intent(this, LoginActivity::class.java)
+            startActivity(intent)
+        }
+
+        btnSignUpSubmit.setOnClickListener{moveToSignupPage()}
     }
 }
