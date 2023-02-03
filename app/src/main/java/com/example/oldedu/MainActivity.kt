6a786ccecb2ad53627a698f9519c79f6e879e715
knxxxn <20211308@sungshin.ac.kr>
2023-02-03 22:01:35 +0900

    main page design edit

--- app/src/main/java/com/example/oldedu/MainActivity.kt
+++ app/src/main/java/com/example/oldedu/MainActivity.kt
@@ -15,18 +15,6 @@ class MainActivity : AppCompatActivity() {
         val view = binding.root
         setContentView(view)
 
-        binding.studentbutton.setOnClickListener {
-            val builder = AlertDialog.Builder(this)
-            builder.setTitle("Do you want Login?")    // 제목
-            val itemList = arrayOf<String>("Login", "No Login")    // 항목 리스트
-            // 항목 클릭 시 이벤트
-            builder.setItems(itemList) { dialog, which ->
-                binding.textView5.text = "${itemList[which]}이 선택되었습니다"
-
-            }
-
-            builder.show()
-        }
 
         teacherbutton.setOnClickListener({
             val intent = Intent(this, LoginActivity::class.java)
