f1a12c24caab44c57395e33fd0a54c19f5c359cf
knxxxn <20211308@sungshin.ac.kr>
2023-02-15 23:37:28 +0900

    dialog edit

--- app/src/main/java/com/example/oldedu/educated2.kt
+++ app/src/main/java/com/example/oldedu/educated2.kt
@@ -1,9 +1,15 @@
 package com.example.oldedu
 
+import android.content.DialogInterface
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AlertDialog
 import kotlinx.android.synthetic.main.activity_add.*
+import kotlinx.android.synthetic.main.activity_add.back
+import kotlinx.android.synthetic.main.activity_add.btn_home
+import kotlinx.android.synthetic.main.activity_educated2.*
+import kotlinx.android.synthetic.main.activity_main.*
 
 class educated2 : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -19,5 +25,23 @@ class educated2 : AppCompatActivity() {
             val intent = Intent(this, educated1::class.java)
             startActivity(intent)
         })
+
+        btn_txt.setOnClickListener {
+            val builder = AlertDialog.Builder(this)
+            builder.setTitle("Login")
+                .setMessage("Do you want to Login?")
+                .setPositiveButton("yes",
+                    DialogInterface.OnClickListener { dialog, id ->
+                        var resultText = "확인"
+                        val intent = Intent(this, LoginActivity::class.java)
+                        startActivity(intent)
+                    })
+                .setNegativeButton("no",
+                    DialogInterface.OnClickListener { dialog, id ->
+                        var resultText = "취소"
+
+                    })
+            builder.show()
+        }
     }
 }
