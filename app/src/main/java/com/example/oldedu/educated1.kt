1d3ac89df54aed5bff9f79357ad89a6a4d34c619
knxxxn <20211308@sungshin.ac.kr>
2023-02-17 23:07:54 +0900

    1

--- app/src/main/java/com/example/oldedu/educated1.kt
+++ app/src/main/java/com/example/oldedu/educated1.kt
@@ -3,7 +3,8 @@ package com.example.oldedu
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.widget.Toast
+import android.util.Log
+import androidx.annotation.Keep
 import com.google.gson.annotations.SerializedName
 import kotlinx.android.synthetic.main.activity_mypage_teacher.*
 import kotlinx.android.synthetic.main.activity_mypage_teacher.btn_back
@@ -12,10 +13,12 @@ import retrofit2.Callback
 import retrofit2.Response
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
+import retrofit2.http.GET
+import retrofit2.http.Path
 import retrofit2.http.Query
 
 
-class educated1 : AppCompatActivity() {
+class educated1Activity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_educated1)
@@ -30,47 +33,61 @@ class educated1 : AppCompatActivity() {
             startActivity(intent)
         })
 
+        val retrofit = Retrofit.Builder().baseUrl("http://34.168.110.14:8080/").addConverterFactory(
+            GsonConverterFactory.create()).build()
+        val service = retrofit.create(educated1Api::class.java)
 
+        val getIntent = getIntent()
+        val heart = getIntent.getIntExtra("heart",0)
+
+        service.getheart(heart).enqueue(object : Callback<educated1> {
+            override fun onResponse(call: Call<educated1>, response: Response<educated1>) {
+                var result = response.body()
+                Log.d("테스트","test"+response)
+
+
+            }
+
+            override fun onFailure(call: Call<educated1>, t: Throwable) {
+                Log.d("retrofit","실패")
+            }
+
+
+        })
 
 
     }
 }
 
-object RetrofitClass {
-    private val retrofit = Retrofit.Builder()
-        .baseUrl("http://34.168.110.14:8080/heartPosts")
-        .addConverterFactory(GsonConverterFactory.create())
-        .build()
-
-    private val _api = retrofit.create(heartInterface::class.java)
-
-
-}
-
-data class heartresult(
-    val result: List<result>
-    )
-
-data class result(
+@Keep
+data class educated1(
     @SerializedName("heart")
-    val title: String,
-    val userId: String,
-    val in_date: String,
     val views: Int,
-    val heart: Int,
-    val declaration: Int,
-    val category: String,
-    val postId: String
 
+    @SerializedName("title")
+    val title: String,
+
+    @SerializedName("userId")
+    val userId: String,
+
+    @SerializedName("in_date")
+    val in_date: String,
+
+    @SerializedName("heart")
+    val heart: Int,
+
+    @SerializedName("declaratation")
+    val declaration: Int,
+
+    @SerializedName("category")
+    val category: String,
+
+    @SerializedName("postId")
+    val postId: String
 )
 
-interface heartInterface{
-
-    fun getResult(@Query("title") title: String,
-                  @Query("in_date") in_date:String,
-                  @Query("category") category: String,
-                  @Query("postId") postId: String
-
-    ): Call<heartresult>
+interface educated1Api {
+    @GET("heartpost/life style")
+    fun getheart(@Path("category") heart: Int): Call<educated1>
 }
 
