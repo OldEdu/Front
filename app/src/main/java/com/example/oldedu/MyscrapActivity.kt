7bae2482ff69d42d2870ada538848a1bfec9d03d
knxxxn <20211308@sungshin.ac.kr>
2023-03-16 00:31:18 +0900

    스크랩 모음집인데 애림언니 엑티비티랑 item_머시기도 같이 추가했는데 이거 맞지?

---
+++ app/src/main/java/com/example/oldedu/MyscrapActivity.kt
@@ -0,0 +1,11 @@
+package com.example.oldedu
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MyscrapActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_myscrap)
+    }
+}
